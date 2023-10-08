34be7dc0a2fa7dfaf455016e4fe1136c1975d790
waranyoo3 <633230013@Webmail.npru.ac.th>
2023-10-08 10:23:34 +0700

    Update 2_page2.py

--- pages/2_page2.py
+++ pages/2_page2.py
@@ -11,6 +11,6 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/acd0c339-c8cb-4796-930f-5c7844bbbc1c/eiEBu3mxcA.json"
+lottie_url_hello = "https://lottie.host/8f612294-26b6-457f-99f9-95113e877b4c/j14usxO0ZF.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello, key="hello")
